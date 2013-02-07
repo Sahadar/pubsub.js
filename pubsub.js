371aa5ac60dcc8ddb1c57801b4a0b3bae238385d
Sahadar <wdsahadar@gmail.com>
2013-02-08 00:58:41 +0100

    Improved pubsub - added unsubscribe and wildcars + tests

--- pubsub.js
+++ pubsub.js
@@ -1,6 +1,17 @@
 'use strict';
 (function() {
 	var _eventObject = {};
+	var _options = {
+		separator : '/',
+		recurrent : false,
+		log       : false
+	}
+
+	function generateId() {
+		var id = Math.random()*1000000000000;
+		return parseInt(id, 10);
+	}
+
 	var pubsub = {
 		/**
 		 * Publish event
@@ -11,20 +22,20 @@
 		 */
 		publish : function(ns_string, args, recurrent, depth) {
 			var ns_string = (typeof ns_string === 'string') ? ns_string : (console.error('ns_string must be string type'), 'randomString'),
-				parts = ns_string.split('.'),
-				recurrent = recurrent || false, // bubbles event throught namespace if true
+				parts = ns_string.split(_options.separator),
+				recurrent = recurrent || _options.recurrent, // bubbles event throught namespace if true
 				nsObject, //Namespace object to which we attach event
 				args = (args) ? args : [],
 				i;
 			
 			function executeCallback(subscribtions) {
 				var i = 0,
-					subscribton = null;
+					subscribtion = null;
 
-				for(i = 0; i < subscribtions.length; i++) {
-					subscribton = subscribtions[i]
-					if(typeof subscribton === 'object' && subscribton != null) {		
-						subscribton.callback.apply(subscribton.object, args);
+				for(subscribtion in subscribtions) {
+					if(typeof subscribtions[subscribtion] === 'object' && subscribtions[subscribtion] != null && subscribtions.hasOwnProperty(subscribtion)) {
+						subscribtion = subscribtions[subscribtion];	
+						subscribtion.callback.apply(subscribtion.object, args);
 					}
 				}
 			}
@@ -32,7 +43,9 @@
 			nsObject = _eventObject;
 			for (i = 0; i < parts.length; i += 1) {
 				if (typeof nsObject[parts[i]] === "undefined") {
-					console.warn('There is no ' + ns_string + ' subscription');
+					if(_options.log) {
+						console.warn('There is no ' + ns_string + ' subscription');
+					}
 					return null;
 				}
 				nsObject = nsObject[parts[i]];
@@ -55,54 +68,62 @@
 		 */
 		subscribe : function(ns_string, callback, givenObject) {
 			var ns_string = (typeof ns_string === 'string') ? ns_string : (console.error('ns_string must be string type'), 'randomString'),
-				parts = ns_string.split('.'),
+				parts = ns_string.split(_options.separator),
 				nsObject, //Namespace object to which we attach event
 				givenObjectSet = (givenObject) ? true : false,
 				givenObject = (givenObjectSet) ? givenObject : callback,
 				eventObject = null,
 				i = 0,
 				k = 0,
-				subscribeArguments = arguments;
+				subscribtionId = null;
 			
 			//Iterating through _eventObject to find proper nsObject
 			nsObject = _eventObject;
 			for (i = 0; i < parts.length; i += 1) {
 				if (typeof nsObject[parts[i]] === "undefined") {
 					nsObject[parts[i]] = {};
-					nsObject[parts[i]]['_events'] = [];
+					nsObject[parts[i]]['_events'] = {};
 				}
 				nsObject = nsObject[parts[i]];
 			}
 			
+			do {
+				subscribtionId = generateId();
+			} while(typeof nsObject['_events'][subscribtionId] === 'object');
+			
 			eventObject = {
+				eventId		: subscribtionId,
 				callback	: callback,
 				object		: givenObject // "this" parameter in executed function
 			};
-			
-			nsObject['_events'].push(eventObject);
-			return [parts.join('.'), eventObject];
+
+			nsObject['_events'][subscribtionId] = eventObject;
+			return [parts.join(_options.separator), eventObject];
 		},
 		unsubscribe : function(subscribeObject) {
 			var ns_string = subscribeObject[0],
-				parts = ns_string.split('.'),
+				parts = ns_string.split(_options.separator),
 				eventObject = subscribeObject[1],
-				nsObject;
+				nsObject,
+				i = 0;
 			
 			//Iterating through _eventObject to find proper nsObject
 			nsObject = _eventObject;
 			for (i = 0; i < parts.length; i += 1) {
 				if (typeof nsObject[parts[i]] === "undefined") {
-					console.error('There is no ' + ns_string + ' subscription');
+					if(_options.log) {
+						console.error('There is no ' + ns_string + ' subscription');
+					}
 					return null;
 				}
 				nsObject = nsObject[parts[i]];
 			}
 			
-			nsObject && $.each(nsObject['_events'], function(functionId){
-				if(this == eventObject) {
-					nsObject['_events'].splice(functionId, 1);
-				}
-			});
+			if(nsObject['_events'][eventObject['eventId']]) {
+				delete nsObject['_events'][eventObject['eventId']];
+			} else if(_options.log) {
+				console.info('There is no such subscribtion: ', subscribeObject);
+			}
 		}
 	};
 
--- test/js/test.js
+++ test/js/test.js
@@ -38,3 +38,53 @@ test("Publish test (many params)", function() {
 	pubsub.publish('hello/world3', [param1, param2]);
 	ok(values.param1 === param1 && values.param2 === param2, 'Values has proper values');
 });
+
+test("Unsubscribe test (basic)", function() {
+	var values = {};
+	var param1 = "some param1";
+	var param2 = "some param2";
+
+	var subscribtion = pubsub.subscribe('hello/world4', function(param1, param2) {
+		values = {
+			'param1' : param1,
+			'param2' : param2
+		};
+	});
+	pubsub.publish('hello/world4', [param1, param2]);
+	ok(values.param1 === param1 && values.param2 === param2, 'Values has proper value');
+	pubsub.unsubscribe(subscribtion);
+	pubsub.publish('hello/world4', [null, null]);
+	ok(values.param1 === param1 && values.param2 === param2, 'Values has proper value');
+});
+
+test("Wildcards test (basic)", function() {
+	var values = {};
+	var param1 = "some param1";
+	var param2 = "some param2";
+
+	var subscribtion = pubsub.subscribe('hello', function(param1, param2) {
+		values = {
+			'param1' : param1,
+			'param2' : param2
+		};
+	});
+	pubsub.publish('hello/world5', [param1, param2], true);
+	ok(values.param1 === param1 && values.param2 === param2, 'Values has proper value');
+	pubsub.unsubscribe(subscribtion);
+});
+
+// test("Wildcards test (more advanced)", function() {
+// 	var values = {};
+// 	var param1 = "some param1";
+// 	var param2 = "some param2";
+
+// 	var subscribtion = pubsub.subscribe('hello', function(param1, param2) {
+// 		values = {
+// 			'param1' : param1,
+// 			'param2' : param2
+// 		};
+// 	});
+// 	pubsub.publish('hello/world5', [param1, param2], true);
+// 	ok(values.param1 === param1 && values.param2 === param2, 'Values has proper value');
+// 	pubsub.unsubscribe(subscribtion);
+// });
